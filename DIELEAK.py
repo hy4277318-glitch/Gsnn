88e9ebfe24912c3d463d702ebc2fe082776fed5a
hy4277318-glitch <hy4277318@gmail.com>
2026-05-05 11:13:55 +0530

    Update DIELEAK.py

--- DIELEAK.py
+++ DIELEAK.py
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 
 BOT_TOKEN        = "7782412259:AAH9D952JvU_i4ma3goBceMZSoxelSCWvwA"
-MONGODB_URI      = "mongodb+srv://himanshuyadavyadav167_db_user:<3QZk2sQZNbTxsH5K>@cluster0.0r5pdj3.mongodb.net/?appName=Cluster0"
+MONGODB_URI      = "mongodb+srv://himanshuyadavyadav167_db_user:3QZk2sQZNbTxsH5K@cluster0.0r5pdj3.mongodb.net/?appName=Cluster0"
 DATABASE_NAME    = os.getenv("himanshu", "attack_bot")
 API_URL          = os.getenv("https://kimstress.st")
 API_KEY          = os.getenv("7d6134479bd735f9c85b0ac129ef5df47 2b11614e6f6eec07a00fcc0240c7ca")
